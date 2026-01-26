d13f5153096c396bb1b8d8b185aff91787b12593
Raab <jasondiwan@gmail.com>
2026-01-26 22:57:57 +0300

    Update HomePage.vue

--- projet-cloud-mobile/src/views/HomePage.vue
+++ projet-cloud-mobile/src/views/HomePage.vue
@@ -1,73 +1,26 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Accueil - Carte et Récap</ion-title>
       </ion-toolbar>
     </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-
-      <button @click="testLogin">Test Login</button>
+    <ion-content class="ion-padding">
+      <p>Bienvenue ! Ici viendra la carte OSM et le récap des signalements.</p>
+      <ion-button expand="block" @click="logout">Déconnexion</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+import { logout } from '@/services/firebaseService';
 
-import { login, getCurrentUser } from '@/services/firebaseService';
-import { getMesSignalements } from '@/services/firebaseService';
+const router = useRouter();
 
-const testLogin = async () => {
-  try {
-    // Utilise un compte créé via web manager
-    const user = await login('test@exemple.com', 'password123');
-    console.log('Login OK:', user);
-    const mesSig = await getMesSignalements();
-    console.log('Mes signalements:', mesSig);
-  } catch (e) {
-    console.error(e);
-  }
+const logout = async () => {
+  await logout();
+  router.push('/login');
 };
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
