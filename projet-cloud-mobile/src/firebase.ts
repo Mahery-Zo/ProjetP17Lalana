65ad4e66a52912336681fb210dbc16f9bfbf8e5a
Raab <jasondiwan@gmail.com>
2026-01-26 21:59:30 +0300

    Create firebase.ts

---
+++ projet-cloud-mobile/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdsICr1naPeL94VP19ULy3Hpm3eeaL35s",
+  authDomain: "projet-cloud-s5-47ed8.firebaseapp.com",
+  projectId: "projet-cloud-s5-47ed8",
+  storageBucket: "projet-cloud-s5-47ed8.firebasestorage.app",
+  messagingSenderId: "311369941585",
+  appId: "1:311369941585:web:3f2f8e18b4278ccb912e70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
